0daf826d87bdca23b2bda1edd6354eeec5361f97
aceslowman <aceslowman@gmail.com>
2018-03-31 15:30:15 -0600

    Working on Polyline class. Basic subdivision working.

--- public/js/main.js
+++ public/js/main.js
@@ -5,24 +5,19 @@ import asCapture from "./utils/asCapture";
 import asDebug from "./utils/asDebug";
 import dat from "dat.gui";
 
-import Box from "./sceneSubjects/Box";
+import Polyline from "./sceneSubjects/Polyline";
 import PointLight from "./sceneSubjects/PointLight";
 
 let gui, manager, debug, capturer;
-let box;
+let lines;
 
 const setup = () => {
   gui      = new dat.GUI();
-  // manager = new asFeedbackManager(gui);
   manager = new asManager(gui);
 
+  lines = new Polyline(manager.scene,manager.eventBus,gui);
 
-  box = new Box(manager.scene,manager.eventBus,gui);
-
-  // manager.subjects = [
-  //   box
-  // ];
-  manager.addSubject(box);
+  manager.addSubject(lines);
 
   debug    = new asDebug();
   capturer = new asCapture(gui, {
